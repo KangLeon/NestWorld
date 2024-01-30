a501ab433b8ec1a5a8ad67a83519e443bfe8cb38
JY <jitengjiao@bytedance.com>
2024-01-31 00:03:13 +0800

    链接数据库
    链接数据库

--- dist/app.module.js
+++ dist/app.module.js
@@ -9,12 +9,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const user_module_1 = require("./user/user.module");
+const typeorm_1 = require("@nestjs/typeorm");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [user_module_1.UserModule],
+        imports: [typeorm_1.TypeOrmModule.forRoot({
+                type: 'mysql',
+                host: '127.0.0.1',
+                port: 3306,
+                username: 'root',
+                password: 'Kangleon28',
+                database: 'xDB',
+                entities: [],
+                synchronize: true,
+                logging: ['error'],
+            }), user_module_1.UserModule],
         controllers: [],
         providers: [],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
+/*
+ * @Author: JY jitengjiao@bytedance.com
+ * @Date: 2024-01-27 17:00:31
+ * @LastEditors: JY jitengjiao@bytedance.com
+ * @LastEditTime: 2024-01-31 00:01:52
+ * @FilePath: /NestWorld/src/app.module.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'Kangleon28',
+    database: 'xDB',
+    entities: [],
+    //同步本地的schema与数据库 -》 初始化的时候去使用
+    synchronize: true,
+    logging: ['error'],
+  }),UserModule],
   controllers: [],
   providers: [],
 })
