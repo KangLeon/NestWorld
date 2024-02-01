5a58bb0bab0cb588a024b54b9461bb779cab91dc
JY <jitengjiao@bytedance.com>
2024-02-01 23:50:08 +0800

    new

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: JY jitengjiao@bytedance.com
  * @Date: 2024-01-27 17:00:31
  * @LastEditors: JY jitengjiao@bytedance.com
- * @LastEditTime: 2024-02-01 23:49:17
+ * @LastEditTime: 2024-02-01 23:50:01
  * @FilePath: /NestWorld/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
