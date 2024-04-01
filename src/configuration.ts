a35427a89f4f629254fc38cb2aeeceb22115b094
JY <397879704@qq.com>
2024-04-01 23:45:14 +0800

    new

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: JY 397879704@qq.com
+ * @Date: 2024-02-03 01:29:55
+ * @LastEditors: JY 397879704@qq.com
+ * @LastEditTime: 2024-04-01 23:44:31
+ * @FilePath: /NestWorld/src/configuration.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { readFileSync } from "fs";
 import * as yaml from "js-yaml";
 import { join } from "path";
