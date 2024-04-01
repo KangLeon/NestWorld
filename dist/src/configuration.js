8611969177b61ad3cdfb60d9a6370ff94bdec77b
JY <397879704@qq.com>
2024-04-02 00:05:49 +0800

    解决一个小问题

--- dist/src/configuration.js
+++ dist/src/configuration.js
@@ -5,9 +5,9 @@ const yaml = require("js-yaml");
 const path_1 = require("path");
 const _ = require("lodash");
 const YAML_COMMON_CONFIG_FILENAME = "config.yml";
-const filePath = (0, path_1.join)(__dirname, "../config", YAML_COMMON_CONFIG_FILENAME);
+const filePath = (0, path_1.join)("config", YAML_COMMON_CONFIG_FILENAME);
 console.log("获取的路径是：" + filePath);
-const envPath = (0, path_1.join)(__dirname, "../config", `config.${process.env.NODE_ENV || "development"}.yml`);
+const envPath = (0, path_1.join)("config", `config.${process.env.NODE_ENV || "development"}.yml`);
 const commonConfig = yaml.load((0, fs_1.readFileSync)(filePath, "utf8"));
 const envConfig = yaml.load((0, fs_1.readFileSync)(envPath, "utf8"));
 exports.default = () => {
--- src/configuration.ts
+++ src/configuration.ts
@@ -2,7 +2,7 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-02-03 01:29:55
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-01 23:57:16
+ * @LastEditTime: 2024-04-02 00:05:27
  * @FilePath: /NestWorld/src/configuration.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,13 +12,12 @@ import { join } from "path";
 import * as _ from "lodash";
 
 const YAML_COMMON_CONFIG_FILENAME = "config.yml";
-const filePath = join(__dirname, "../config", YAML_COMMON_CONFIG_FILENAME);
+const filePath = join("config", YAML_COMMON_CONFIG_FILENAME);
 
 console.log("获取的路径是：" + filePath)
 
 const envPath = join(
-  __dirname,
-  "../config",
+  "config",
   `config.${process.env.NODE_ENV || "development"}.yml`,
 );
 
