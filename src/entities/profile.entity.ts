2a3c5fc7db5374ba8088e01735d7c5c31e93c28a
JY <397879704@qq.com>
2024-04-02 10:39:51 +0800

    项目结构的整理

--- src/entities/profile.entity.ts
+++ src/entities/profile.entity.ts
@@ -6,24 +6,30 @@
  * @FilePath: /NestWorld/src/user/profile.entiry.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "./user.entity";
+import { User } from "src/entities/user.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
 @Entity()
-export class Profile { 
-    @PrimaryGeneratedColumn()
-    id: number
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    gender: number
+  @Column()
+  gender: number;
 
-    @Column()
-    photo: string
+  @Column()
+  photo: string;
 
-    @Column()
-    address: string
+  @Column()
+  address: string;
 
-    @OneToOne(() => User)
-    @JoinColumn()
-    user: User
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,23 +6,37 @@
  * @FilePath: /NestWorld/src/user/user.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return {
-      code: 0,
-      data: [],
-      msg: '请求用户列表成功',
-    };
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.userRepository.find();
   }
 
-  addUser() {
-    return {
-      code: 0,
-      data: {},
-      msg: '新增用户成功',
-    };
+  find(username: string) {
+    return this.userRepository.findOne({
+      where: { username },
+    });
+  }
+
+  async create(user: User) {
+    const userTmp = await this.userRepository.create(user);
+    return this.userRepository.save(userTmp);
+  }
+
+  async update(id: number, user: Partial<User>) {
+    return this.userRepository.update(id, user);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
   }
 }
